52744fb69fcf94d7ee191fdf39f9e9fed0cd2458
JubairAnjumRony <rony.ict.iu@gmail.com>
2024-10-29 16:41:19 +0600

    Header footer const Outlet middle

---
+++ src/components/About/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+
+
+const Contact = () => {
+    return (
+        <div>
+          <h2>For any support </h2>
+          <p>Contact us</p>  
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/main.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Header from './components/Header/Header.jsx';
+import Home from './components/Home/Home.jsx';
+import Contact from './components/About/Contact/Contact.jsx';
+import About from './components/About/About.jsx';
+import Footer from './components/Footer/footer.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Home></Home>,
+    children:[{
+      path:'/about',
+      element:<About></About>
+    },
+    {
+      path:'/contact',
+      element:<Contact></Contact>
+    },
+    {
+      path:'/footer',
+      element:<Footer></Footer>
+    }
+  ]
+  },
+  // {
+  //   path:'/about',
+  //   element:<div>I am in the about Page</div>
+  // },
+  // {
+  //   path:'/contact',
+  //   element:<div>I am in the contact page <br></br> Contact me you dumb ass</div>
+  // },
+  // {
+  //   path:'header',
+  //   element:<Header></Header>
+  // }
+  
+
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
